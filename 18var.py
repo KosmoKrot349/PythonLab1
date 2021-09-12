39f7dbe7ed2befac9eec95a6201ae880d545e7b5
KosmoKrot349 <82542440+KosmoKrot349@users.noreply.github.com>
2021-09-12 16:20:19 +0300

    Update 18var.py

--- 18var.py
+++ 18var.py
@@ -2,8 +2,12 @@ import math
 
 
 a=0
+print('input n')
+n=float(input())
+print('input x')
 x=float(input())
 
-for i in range(10):
+for i in range(n):
         a+= math.pow((x-1),i)/math.factorial(i)
 print(a)
+
