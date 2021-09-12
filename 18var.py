5e2c8854418b8c92feb936e9ccb43042e46f7091
KosmoKrot349 <82542440+KosmoKrot349@users.noreply.github.com>
2021-09-12 17:30:09 +0300

    Update 18var.py

--- 18var.py
+++ 18var.py
@@ -1,13 +1,22 @@
 import math
 
+def fact(n):
+        factor=1;
+        for i in range(1,n+1):
+                factor*=i
+        return factor
 
-a=0
-print('input n')
-n=float(input())
-print('input x')
-x=float(input())
+try:
+        a=0
+        print('input n')
+        n=int(input())
+        print('input x')
+        x=float(input())
 
-for i in range(n):
-        a+= math.pow((x-1),i)/math.factorial(i)
-print(a)
+        for i in range(n):
+                a+= math.pow((x-1),i)/fact(i)
+        print(a)
+except ValueError:
+        print("some error")
+
 
